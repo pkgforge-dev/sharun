95b77999dadaf3dfd3f5b8fbf6783008b457fb4a
Samueru-sama <xdglawyer@outlook.com>
2026-06-13 03:06:51 -0400

    simplify CI

--- .github/create_aio_sharun.sh
+++ .github/create_aio_sharun.sh
@@ -3,11 +3,11 @@ set -e
 
 ARCH="$(uname -m)"
 
-apk add bash file binutils patchelf findutils grep sed coreutils strace which wget tar gzip
+apk add bash file patchelf findutils grep sed coreutils strace which wget
 
-BINS="bash patchelf strip strace find file grep sed awk md5sum \
-xargs kill rm cp ln mv sleep echo readlink chmod sort tr printenv \
-cut mkdir basename dirname uname wget tail date tar gzip ls"
+BINS="bash patchelf strace find file grep sed awk \
+xargs rm cp ln mv sleep readlink chmod sort tr \
+cut mkdir basename dirname uname wget tail"
 
 BINS_PATHS=
 for bin in $BINS
@@ -19,7 +19,7 @@ wget "https://github.com/VHSgunzo/wrappe/releases/latest/download/wrappe-$ARCH" 
 chmod +x "$WRAPPE"
 
 SHARUN="$PWD/sharun-$ARCH" \
-"$PWD/lib4bin" -k -o -c 22 -s -g $BINS_PATHS "$WRAPPE"
+"$PWD/lib4bin" -o -c 22 -g $BINS_PATHS "$WRAPPE"
 
 mv sharun "sharun-$ARCH-aio"
 rm -f wrappe*
